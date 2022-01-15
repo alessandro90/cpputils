e3828ed59030600ec2dceecfb94199e6efe6103c
Alessandro <alessandro.ceccato.3@gmail.com>
2022-01-15 21:51:39 +0100

    Better cmake directoty structure

---
+++ tests/opt_ext_test.cpp
@@ -0,0 +1,11 @@
+#include "catch2/catch_test_macros.hpp"
+#include "types/number.hpp"
+#include <vector>
+
+using namespace cpputils;
+
+
+TEST_CASE("opt test", "[literal]") {
+    auto const i = 10_u32;
+    REQUIRE(1 == 1);
+}
