70f2e1ab31a5cd50897ee3a7a2868ffcde24fea2
Alessandro <alessandro.ceccato.3@gmail.com>
2022-10-09 21:29:33 +0200

    Update cpputils.hpp

--- src/include/cpputils/cpputils.hpp
+++ src/include/cpputils/cpputils.hpp
@@ -1,17 +1,30 @@
-#ifndef CPPUTILS_CPPUTILS_HPP
-#define CPPUTILS_CPPUTILS_HPP
+#ifndef CPPUTILS_INCLUDE_ALL_IN_ONE_HPP
+#define CPPUTILS_INCLUDE_ALL_IN_ONE_HPP
 
+#include "functional/composition.hpp"
 #include "functional/enumerate.hpp"
+#include "functional/expected.hpp"
+#include "functional/operator_sections.hpp"
 #include "functional/opt_ext.hpp"
+#include "functional/pipeable.hpp"
 #include "functional/tovector.hpp"
+#include "functional/utils.hpp"
 #include "functional/zip.hpp"
 #include "functional/zip_with.hpp"
 
+
+#include "types/lazy.hpp"
 #include "types/number.hpp"
+#include "types/optional_ref.hpp"
 #include "types/static_string.hpp"
 
 #include "meta/traits.hpp"
 #include "meta/typelist.hpp"
+
+#include "misc/benchmark.hpp"
+#include "misc/container_views.hpp"
+#include "misc/system_macros.hpp"
+#include "misc/visitor.hpp"
 
 
 #endif
